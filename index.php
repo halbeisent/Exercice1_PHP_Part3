6a56f10e9593a87c59413f57453c0d6749ff1d1f
halbeisent <dev@halbeisen.fr>
2018-05-16 16:06:23 +0200

    Exercice 1 Partie 3 PHP Terminé et Corrigé

--- index.php
+++ index.php
@@ -9,9 +9,10 @@
   <?php
   // On initlalise la variable count à 0
   $count = 0;
-  while ($count <= 10) {//Tant que la valeur de la variable $count est inférieure à 10
-    echo $count . '<br />';//On affiche son contenu en sautant une ligne
-    $count++; //Puis on l'incrémente
+  //Tant que la valeur de la variable $count est inférieure à 10
+  while ($count <= 10) { ?>
+    <p><?php echo $count;?></p>
+    <?php $count++; //Puis on l'incrémente
   }
   ?>
   </body>
